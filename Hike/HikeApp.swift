a49f4a3587b7a51ecbb0965161f8c40fb305217b
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-10-18 10:17:50 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Cristina on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
