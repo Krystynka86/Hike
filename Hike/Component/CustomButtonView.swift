6f2d9e3135f07a2b41b65b943aa4486b923d00ae
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-10-19 21:29:25 +0200

    add header component

---
+++ Hike/Component/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  Hike
+//
+//  Created by Cristina on 2023-10-19.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
